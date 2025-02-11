9a2d1ba7df6d6d4f5e19d0216dc6168be14d1a09
NiSHAN808 <nishan4b@gmail.com>
2025-02-11 13:30:45 +0545

    last

--- index.js
+++ index.js
@@ -8,14 +8,9 @@ const path=require('path');
 const { json } = require('stream/consumers');
 let htm_pat=path.join(__dirname,"/index.html");
 let playwf=path.join(__dirname,"/Views/playwf.html");
-let gamer1;
-let gamer2;
-let gamer1_name;
-let gamer2_name;
 
-let gameno=0;
-let totalplayers=0;
-//arr[0]={namee:"nishan", add:12};
+
+
 
 app.get("/",(req,res)=>{
     res.sendFile(htm_pat);
@@ -34,16 +29,15 @@ var room_id=0;
 io.on('connection',Socket=>{
 Socket.on("i-pn-s",(name)=>{
 players.push(Socket.id);   
-numbers_of_players++; 
+numbers_of_players++;
 console.log(numbers_of_players);   
 
 if(numbers_of_players%2===0){
-   // console.log(numbers_of_players-2);
-   // console.log(numbers_of_players-1);
     let x=numbers_of_players;
     Socket.emit(Socket.id,waiting_player,room_id.toString(),x-2);
- Socket.broadcast.emit(players[x-=2],name,room_id.toString(),x-1);
-  //Socket.on(room_id.toString(),player_no,position=>{
+ const y=x-1;
+    console.log("y="+y);
+ Socket.broadcast.emit(players[x-2],name,room_id.toString(),x-1);
     console.log("after emit",numbers_of_players);    
 }else{
 waiting_player=name;
