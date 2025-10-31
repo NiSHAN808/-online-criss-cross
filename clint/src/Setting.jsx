56d52ad5f453f9fbe64b03f66d49aed72ab58ec6
NiSHAN808 <nishan4b@gmail.com>
2025-10-31 23:58:57 +0545

    setting templeater for name change

--- clint/src/Setting.jsx
+++ clint/src/Setting.jsx
@@ -1,7 +1,21 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 const Setting = () => {
-  return <div>Setting</div>;
+  const [name, setName] = useState(null);
+
+  useEffect(() => {
+    setName(localStorage.getItem("name"));
+  });
+
+  return (
+    <div>
+      Setting
+      <div>{name}</div>
+      <div>
+        <input type="txt"></input>
+        <button>change</button>
+      </div>
+    </div>
+  );
 };
 
 export default Setting;
