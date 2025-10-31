357460a9f6648988d2207c208de49d779dfdb2f4
NiSHAN808 <nishan4b@gmail.com>
2025-11-01 00:26:42 +0545

    setting page that can change the name is created

--- clint/src/Setting.jsx
+++ clint/src/Setting.jsx
@@ -1,18 +1,27 @@
 import React, { useState, useEffect } from "react";
 const Setting = () => {
   const [name, setName] = useState(null);
+  const [value, setValue] = useState("");
 
   useEffect(() => {
     setName(localStorage.getItem("name"));
   });
 
+  const handleChange = (e) => {
+    setValue(e.target.value); // update state with input value
+  };
+
+  const handlesummitChange = () => {
+    localStorage.setItem("name", value);
+    setName(value);
+  };
   return (
     <div>
       Setting
       <div>{name}</div>
       <div>
-        <input type="txt"></input>
-        <button>change</button>
+        <input value={value} onChange={handleChange} type="txt"></input>
+        <button onClick={handlesummitChange}>change</button>
       </div>
     </div>
   );
