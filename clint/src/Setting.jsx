6c12fc81976f5977cf49ca46c5430b57bd6c7231
NiSHAN808 <nishan4b@gmail.com>
2025-11-03 23:54:15 +0545

    made the setting look good for mobile

--- clint/src/Setting.jsx
+++ clint/src/Setting.jsx
@@ -19,21 +19,32 @@ const Setting = () => {
   return (
     <div className="min-h-screen w-full bg-gradient-to-r from-green-700 to-yellow-400">
       <Navbar />
-      Setting
-      <div>{name}</div>
-      <div>
-        <input
-          className="bg-stone-500"
-          value={value}
-          onChange={handleChange}
-          type="txt"
-        ></input>
-        <button
-          className="bg-stone-800 text-white"
-          onClick={handlesummitChange}
-        >
-          change
-        </button>
+      <div className=" w-[100vw] inline-flex  justify-center border-2">
+        <div className=" h-[50vw] w-[85vw] ">
+          <h3 className="text-[2rem] w-full inline-flex justify-center mt-[5vw]">
+            {" "}
+            Setting
+          </h3>
+          <div className="w-full inline-flex justify-center mt-15">{name}</div>
+          <div>
+            <div className="inline-flex w-full justify-center mt-[5vw]">
+              <div className="inline-flex bg-green-400 rounded-md w-content">
+                <input
+                  className=""
+                  value={value}
+                  onChange={handleChange}
+                  type="txt"
+                ></input>
+                <button
+                  className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 m-1 mt-1.5 mb-1.5   dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                  onClick={handlesummitChange}
+                >
+                  change
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
